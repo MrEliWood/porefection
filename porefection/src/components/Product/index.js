77e1ae4e06702a713ba0f7c43c7b5c550c1c37fb
Eli Wood <mreliwood1@gmail.com>
2023-01-09 15:16:23 -0800

    fixing p tag bug in suggested use and only allowing one open accordian at a time

--- porefection/src/components/Product/index.js
+++ porefection/src/components/Product/index.js
@@ -1,76 +1,71 @@
 import './style.css';
 
 function Product({ index, routine, getRoutine, product }) {
+	const handleAccordian = (e) => {
+		// transform carrot
+		let carrot = e.target.lastChild;
+		carrot.style.transform === 'scaleY(-1)' ? (carrot.style.transform = 'scaleY(1)') : (carrot.style.transform = 'scaleY(-1)');
 
-    const handleAccordian = (e) => {
+		// close all other panels
+		let accordian = document.getElementsByClassName('accordian');
 
-        let carrot = e.target.lastChild;
+		const closeAllPanels = () => {
+			for (let menu of accordian) {
+				menu.style.maxHeight = null;
+			}
+		};
 
-        console.log('CARROT', e.target)
+		// open panel
+		let panel = e.target.nextElementSibling;
 
-        carrot.style.transform === 'scaleY(-1)'
-            ? carrot.style.transform = 'scaleY(1)'
-            : carrot.style.transform = 'scaleY(-1)';
+		if (panel.style.maxHeight) {
+			closeAllPanels();
+		} else {
+			closeAllPanels();
+			panel.style.maxHeight = panel.scrollHeight + 'px';
+		}
+	};
 
-        let panel = e.target.nextElementSibling;
+	// remove product
+	const removeProduct = () => {
+		let removeConfirmed = window.confirm('Are you sure you want to remove this product from your routine?');
 
-        panel.style.maxHeight
-            ? panel.style.maxHeight = null
-            : panel.style.maxHeight = panel.scrollHeight + "px";
+		if (removeConfirmed) {
+			document.getElementById(index).classList.add('remove-product-animation');
 
-    };
+			setTimeout(() => {
+				const updatedRoutine = JSON.stringify(routine);
+				const productString = JSON.stringify(product);
+				localStorage.setItem('Porefection Skincare Routine', updatedRoutine.replace(productString, ''));
+				getRoutine();
+			}, 2000);
+		}
+	};
 
-        // remove product
-        const removeProduct = () => {
+	return (
+		<div className={'product-card'}>
+			{window.innerWidth > 768 && <img className='product-image' src={product.currentSku?.skuImages?.image1500} alt={product.imageAltText}></img>}
 
-            let removeConfirmed = window.confirm('Are you sure you want to remove this product from your routine?');
-    
-            if (removeConfirmed) {
-    
-                document.getElementById(index).classList.add("remove-product-animation");
-    
-                setTimeout(() => {
-                    const updatedRoutine = JSON.stringify(routine);
-                    const productString = JSON.stringify(product);
-                    localStorage.setItem('Porefection Skincare Routine', updatedRoutine.replace(productString, ''));
-                    getRoutine();
-                }, 2000);
-    
-            };
-    
-        };
+			<div className='product-details'>
+				<div className='product-banner'>
+					{window.innerWidth <= 768 && <img className='product-image' src={product.currentSku?.skuImages?.image1500} alt={product.imageAltText}></img>}
 
-    return (
+					<div className='product-headline'>
+						<div className='product-brand-category'>
+							<p className='product-brand'>{product.brand.displayName}</p>
+							<h4>|</h4>
+							<p className='product-category'>{product.parentCategory.displayName}</p>
+						</div>
+						<h2 className='product-name'>{product.displayName}</h2>
+					</div>
 
-        <div className={"product-card"}>
+					<svg className='remove-product-btn' viewBox='0 0 24 24' width='12' height='12' onClick={removeProduct}>
+						<line x1='2' y1='2' x2='22' y2='22' />
+						<line x1='2' y1='22' x2='22' y2='2' />
+					</svg>
+				</div>
 
-            {window.innerWidth > 768 && <img className="product-image" src={product.currentSku?.skuImages?.image1500} alt={product.imageAltText}></img>}
-
-            <div className="product-details">
-
-                <div className="product-banner">
-
-                    {window.innerWidth <= 768 && <img className="product-image" src={product.currentSku?.skuImages?.image1500} alt={product.imageAltText}></img>}
-
-                    <div className="product-headline">
-
-                        <div className="product-brand-category">
-                            <p className="product-brand">{product.brand.displayName}</p>
-                            <h4>|</h4>
-                            <p className="product-category">{product.parentCategory.displayName}</p>
-                        </div>
-                        <h2 className="product-name">{product.displayName}</h2>
-
-                    </div>
-
-                    <svg className="remove-product-btn" viewBox="0 0 24 24" width="12" height="12" onClick={removeProduct}>
-                        <line x1="2" y1="2" x2="22" y2="22" />
-                        <line x1="2" y1="22" x2="22" y2="2" />
-                    </svg>
-
-                </div>
-
-                {/* <div className="day-night-icons">
+				{/* <div className="day-night-icons">
 
                     <svg className="day-icon" viewBox="0 0 24 24" width="30" height="30" onClick={() => removeProduct(product)}>
                         <circle cx="12" cy="12" r="6" fill="none" />
@@ -94,70 +89,94 @@ function Product({ index, routine, getRoutine, product }) {
 
                 </div> */}
 
-                <h3 className="product-detail-header" onClick={handleAccordian}>
-                    Description
-                    <svg className="accordian-carrot" viewBox="0 0 24 24" width="12" height="12">
-                        <polyline points="2 7, 12 17, 22 7" />
-                    </svg>
-                </h3>
+				<h3 className='product-detail-header' onClick={handleAccordian}>
+					Description
+					<svg className='accordian-carrot' viewBox='0 0 24 24' width='12' height='12'>
+						<polyline points='2 7, 12 17, 22 7' />
+					</svg>
+				</h3>
 
-                <ul className="accordian description">
-                    {product.shortDescription.replace(/<b>|<\/b>/g, '').split('<br>').map((line, index) => {
-                        return (
-                            <li key={index} className="description-line">
-                                {line.replace(/What it is/g, '').split(':').map((item, i) => {
-                                    return (<p key={i} className="description-item" style={i === 0 ? { fontWeight: '500' } : {}}>{item}</p>)
-                                })}
-                            </li>
-                        );
-                    })}
-                </ul>
+				<ul className='accordian description'>
+					{product.shortDescription
+						.replace(/<b>|<\/b>|<p>|<\/p>/g, '')
+						.split('<br>')
+						.map((line, index) => {
+							return (
+								<li key={index} className='description-line'>
+									{line
+										.replace(/What it is/g, '')
+										.split(':')
+										.map((item, i) => {
+											return (
+												<p key={i} className='description-item' style={i === 0 ? { fontWeight: '500' } : {}}>
+													{item}
+												</p>
+											);
+										})}
+								</li>
+							);
+						})}
+				</ul>
 
-                <h3 className="product-detail-header" onClick={handleAccordian}>
-                    Suggested Use
-                    <svg className="accordian-carrot" viewBox="0 0 24 24" width="12" height="12">
-                        <polyline points="2 7, 12 17, 22 7" />
-                    </svg>
-                </h3>
+				<h3 className='product-detail-header' onClick={handleAccordian}>
+					Suggested Use
+					<svg className='accordian-carrot' viewBox='0 0 24 24' width='12' height='12'>
+						<polyline points='2 7, 12 17, 22 7' />
+					</svg>
+				</h3>
 
-                <ol className="accordian suggested-use">
-                    {product.suggestedUsage.replace(/<br>|<b>|<\/b>/g, '').replace(/&rsquo;/g, `'`).split('-').map((line, index) => {
-                        return (
-                            index > 0 && <li key={index} className="use-line"><p>{line}</p></li>
-                        );
-                    })}
-                </ol>
+				<ol className='accordian suggested-use'>
+					{product.suggestedUsage
+						.replace(/<br>|<b>|<\/b>|<p>|<\/p>/g, '')
+						.replace(/&rsquo;/g, `'`)
+						.split('-')
+						.map((line, index) => {
+							return (
+								index > 0 && (
+									<li key={index} className='use-line'>
+										<p>{line}</p>
+									</li>
+								)
+							);
+						})}
+				</ol>
 
-                <h3 className="product-detail-header" onClick={handleAccordian}>
-                    Ingredient Breakdown
-                    <svg className="accordian-carrot" viewBox="0 0 24 24" width="12" height="12">
-                        <polyline points="2 7, 12 17, 22 7" />
-                    </svg>
-                </h3>
+				<h3 className='product-detail-header' onClick={handleAccordian}>
+					Ingredient Breakdown
+					<svg className='accordian-carrot' viewBox='0 0 24 24' width='12' height='12'>
+						<polyline points='2 7, 12 17, 22 7' />
+					</svg>
+				</h3>
 
-                <ul className="accordian ingredient-breakdown">
-                    {product.currentSku.ingredientDesc.split('Clean at Sephora')[0].replace(/, Inactive Ingredients|, Inactive Ingredients<br>/g, '<br>').replace(/<br><\/b>/g, ':').replace(/<b>|<\/b>| &ndash/g, '').split('<br>').map((line, index) => {
-                        while (line.charAt(0) === '-') {
-                            line = line.substring(1);
-                        }
-                        return (
-                            line && <div className="ingredient">
-                                {line.split(':').map((item, i) => {
-                                    return (
-                                        <p key={index + '.' + i} className="ingredient-line">{item}</p>
-                                    );
-                                })}
-                            </div>
-                        );
-                    })}
-                </ul>
-
-            </div>
-
-        </div>
-
-    );
-
-};
+				<ul className='accordian ingredient-breakdown'>
+					{product.currentSku.ingredientDesc
+						.split('Clean at Sephora')[0]
+						.replace(/, Inactive Ingredients|, Inactive Ingredients<br>/g, '<br>')
+						.replace(/<br><\/b>/g, ':')
+						.replace(/<b>|<\/b>|<p>|<\/p>| &ndash/g, '')
+						.split('<br>')
+						.map((line, index) => {
+							while (line.charAt(0) === '-') {
+								line = line.substring(1);
+							}
+							return (
+								line && (
+									<div className='ingredient'>
+										{line.split(':').map((item, i) => {
+											return (
+												<p key={index + '.' + i} className='ingredient-line'>
+													{item}
+												</p>
+											);
+										})}
+									</div>
+								)
+							);
+						})}
+				</ul>
+			</div>
+		</div>
+	);
+}
 
 export default Product;
